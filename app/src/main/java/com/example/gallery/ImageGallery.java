03940da4295c9ab10fb5547b1b9c765c862c7388
RealGuzu <45natewilson@gmail.com>
2024-02-22 10:41:35 -0500

    COmmit

--- app/src/main/java/com/example/gallery/ImageGallery.java
+++ app/src/main/java/com/example/gallery/ImageGallery.java
@@ -1,4 +1,30 @@
 package com.example.gallery;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ImageGallery {
+
+        public static ArrayList<String> listOfImages(Context context) {
+            Uri uri;
+            Cursor cursor;
+            int column_index_data, column_index_folder_name;
+            ArrayList<String> listOfAllImages = new ArrayList<>();
+
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+            String[] projection = {MediaStore.MediaColumns.DATA,
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+
+            String orderBy = MediaStore.Video.Media.DATE_TAKEN;
+
+
+
+
+        }
 }
